0254b2d9cf0f28877aaaf16b932243006ec81956
Mikheull <mikhael.bailly1@orange.fr>
2019-12-15 22:29:18 +0100

    add map click send socket

--- public/js/map.js
+++ public/js/map.js
@@ -1,47 +1,91 @@
+$(document).on('change', '.mapboxgl-ctrl-geocoder--input', function() {
+    
+    let query = $(this).val();
+    socket.emit('search_velib', type = 'coordones', value = query);
+    
+});
+
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlraGV1bGwiLCJhIjoiY2ppa24wbnVmMjAxZzNxcXAzbGxvcHdwcSJ9.PRr0Bp0Y-i2xUrJ5cBvbPw';
+
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/light-v10', //hosted style id
     center: [2.3797426, 48.8960154], // starting position
     zoom: 10 // starting zoom
 });
-
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
-map.addControl(new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-}));
+ 
+// initialize the map canvas to interact with later
+var canvas = map.getCanvasContainer();
 
 
-
-map.on("load", function () {
-    map.addLayer({
-        "id": "places",
-        "type": "symbol",
-        "source": {
-            "type": "geojson",
-            "data": {
-                "type": "FeatureCollection",
-                "features": [
-                    {
-                        "type": "Feature",
-                        "properties": {
-                            "description": "Vous êtes ici",
-                            "icon": "town-hall"
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [2.3797426, 48.8960154]
-                        }
-                    }
-                ]
-            }
+// Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
         },
-        "layout": {
-        "icon-image": "{icon}-15",
-        "icon-allow-overlap": true
-        }
+        trackUserLocation: true
+    })
+);
+map.addControl(
+    new mapboxgl.NavigationControl()
+);
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
+
+
+
+
+map.on('click', function(e) {
+    socket.emit('search_velib', type = 'coordones_map', value = e.lngLat);
+
+    var coordsObj = e.lngLat;
+    canvas.style.cursor = '';
+    var coords = Object.keys(coordsObj).map(function(key) {
+        return coordsObj[key];
     });
-    
+    var end = {
+        type: 'FeatureCollection',
+        features: [{
+            type: 'Feature',
+            properties: {},
+            geometry: {
+            type: 'Point',
+            coordinates: coords
+            }
+        }]
+    };
+    if (map.getLayer('end')) {
+        map.getSource('end').setData(end);
+    } else {
+        map.addLayer({
+            id: 'end',
+            type: 'circle',
+            source: {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [{
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                    type: 'Point',
+                    coordinates: coords
+                }
+                }]
+            }
+            },
+            paint: {
+                'circle-radius': 5,
+                'circle-color': '#4c6ef5'
+            }
+        });
+    }
 });
+  
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
+const getJSON = require('get-json')
 
 const stations_obj = new (require('../model/Stations'))()
 
 
 router.use(async (req, res, next) => {
+	let data_filter;
 
 	req.io.sockets.on('connection', function (socket) {
 
 		socket.on('search_velib', function(type, value)  {
 			console.log(type, value);
+			getJSON('http://localhost:3000/api/?'+ type +'=' + value, function(error, response){
+				data_filter = response;
+			})
+			console.log(data_filter);
+
 		});
 	
 	});
