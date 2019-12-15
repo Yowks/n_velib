ee9c90aa24004b4e46ca2851faebd4659dab0347
Mikheull <mikhael.bailly1@orange.fr>
2019-12-15 21:28:41 +0100

    router api parameters

--- routes/api.js
+++ routes/api.js
@@ -5,17 +5,37 @@ const stations_obj = new Stations();
 
 
 /* GET api page. */
-router.get('/', function(req, res) {
-	res.send({'code': 404, 'message': 'Requete invalide'});
+router.get('/', async function(req, res) {
+	let code, message, filter_type, filter_val;
+
+	if (req.query.adress) {
+		code = 200;
+		message = 'Résultat avec l\'adresse';
+		filter_type = 'adress';
+		filter_val = req.query.adress;
+	}
+	else if (req.query.coordones) {
+		code = 200;
+		message = 'Résultat avec les coordones';
+		filter_type = 'coordones';
+		filter_val = req.query.coordones;
+	}
+	else{
+		code = 404;
+		message = 'Aucun résultats';
+	}
+
+
+	// la fonction getData récupère tout, a toi de faire passer les paramètres dans la fonction et mettre les filtres dans la requete
+	const data = await stations_obj.getData();
+
+	res.send({
+		'code': code, 
+		'message': message, 
+		'type de filtre': filter_type,
+		'valeur du filtre': filter_val,
+		data});
 });
 
-
-
-router.get('/:adress', async function (req, res) {
-	let adress = req.params.adress;
-	const data = await stations_obj.getData();
-	res.send({'code': 200, 'message': 'OK', data});
-	
-})
 
 module.exports = router;
